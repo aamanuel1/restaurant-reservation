abce0e3dc949e69b62df594cfe56d79ce7cea746
Aaron Manuel <aaron.manuel@ucalgary.ca>
2023-11-11 06:40:37 -0700

    Add StaffRepo class and establish repo package

---
+++ restaurantbooking/src/main/java/com/project/restaurantbooking/repo/StaffRepository.java
@@ -0,0 +1,13 @@
+package com.project.restaurantbooking.repo;
+
+import com.project.restaurantbooking.entity.Staff;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface StaffRepository extends JpaRepository<Staff, Long> {
+
+    Optional<Staff> findUserByStaffId(Long staffId);
+
+    Optional<Staff> findByUsername(String username);
+}

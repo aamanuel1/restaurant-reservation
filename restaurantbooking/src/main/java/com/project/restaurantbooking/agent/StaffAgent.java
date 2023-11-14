aa55035547305b8f8f2a3e6be97c97292770c978
aamanuel1 <aaron.manuel@ucalgary.ca>
2023-11-14 13:07:12 -0700

    Working skeleton version of staff agent login/create_agent

--- restaurantbooking/src/main/java/com/project/restaurantbooking/agent/StaffAgent.java
+++ restaurantbooking/src/main/java/com/project/restaurantbooking/agent/StaffAgent.java
@@ -2,14 +2,23 @@ package com.project.restaurantbooking.agent;
 
 
 import com.project.restaurantbooking.repo.StaffRepository;
+import jade.core.Agent;
 import org.springframework.stereotype.Service;
 
 @Service
-public class StaffAgent {
+public class StaffAgent extends Agent {
 
     private final StaffRepository staffRepository;
 
+    public StaffAgent(){
+        this.staffRepository = null;
+    }
+
     public StaffAgent(StaffRepository staffRepository){
         this.staffRepository = staffRepository;
+    }
+
+    protected void setup(){
+        System.out.println("Testing. Placeholder behaviour");
     }
 }
--- restaurantbooking/src/main/java/com/project/restaurantbooking/controller/StaffController.java
+++ restaurantbooking/src/main/java/com/project/restaurantbooking/controller/StaffController.java
@@ -1,9 +1,56 @@
 package com.project.restaurantbooking.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import com.project.restaurantbooking.agent.StaffAgent;
+import com.project.restaurantbooking.entity.Staff;
+import jade.core.AgentContainer;
+import jade.wrapper.AgentController;
+import jade.wrapper.ContainerController;
+import jade.wrapper.StaleProxyException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+
+import java.util.Optional;
 
 @RestController
 @CrossOrigin({"*"})
 public class StaffController {
+
+    @Autowired
+    private StaffAgent staffAgent;
+
+    @Autowired
+    public StaffController(StaffAgent staffAgent){
+        this.staffAgent = staffAgent;
+    }
+
+    @GetMapping({"/"})
+    public String index(){
+        return "Welcome to the restaurant staff portal. Please login to continue.";
+    }
+
+    @PostMapping("/api/v1/login")
+    public Optional<Staff> login(@RequestParam String username, @RequestParam String password){
+        //Create new staff agent with authenticate ability.
+        Runtime runtime = Runtime.instance();
+        Profile profile = new ProfileImpl();
+        profile.setParameter(Profile.MAIN_HOST, "localhost");
+        ContainerController container = runtime.createMainContainer(profile);
+
+        try{
+            AgentController agentController = container.createNewAgent("testAgent", "com.project.restaurantbooking.agent.StaffAgent", null);
+        } catch (StaleProxyException e){
+            e.printStackTrace();
+        }
+        //Run authenticate function on new staff agent.
+
+        //Return staff agent if successful.
+        return null;
+    }
+
+
+
 }

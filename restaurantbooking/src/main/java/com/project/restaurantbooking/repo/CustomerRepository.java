0bccf6f7b34e5cf1ce8543e15d844825ec5524fd
aamanuel1 <aaron.manuel@ucalgary.ca>
2023-11-23 01:02:07 -0700

    Fixed a issue with a wrong refernce to user id in Customer

--- restaurantbooking/src/main/java/com/project/restaurantbooking/repo/CustomerRepository.java
+++ restaurantbooking/src/main/java/com/project/restaurantbooking/repo/CustomerRepository.java
@@ -7,6 +7,6 @@ import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
-    Optional<Customer> findByCustomerId(Long customerId);
+    Optional<Customer> findByUserId(Long userId);
 
 }

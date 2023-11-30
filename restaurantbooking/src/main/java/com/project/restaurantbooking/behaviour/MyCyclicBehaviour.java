65c0c8492ef5d62818497ddd17c86d1b6f67ab36
Rashid Bawah <arbawah1@gmail.com>
2023-11-30 04:59:29 -0700

    deleted scratch work

--- restaurantbooking/src/main/java/com/project/restaurantbooking/behaviour/MyCyclicBehaviour.java
+++
@@ -1,9 +0,0 @@
-package com.project.restaurantbooking.behaviour;
-
-import jade.core.behaviours.CyclicBehaviour;
-
-public class MyCyclicBehaviour extends CyclicBehaviour {
-    public void action() {
-        // perform operation Y
-    }
-}

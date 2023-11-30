f6bb4cfbf37ab81333b4a3e4915050d80e504204
Rashid Bawah <arbawah1@gmail.com>
2023-11-30 05:02:23 -0700

    deleted scratch work (#2)
    deleted scratch work

--- restaurantbooking/src/main/java/com/project/restaurantbooking/behaviour/MyOneShotBehaviour.java
+++
@@ -1,9 +0,0 @@
-package com.project.restaurantbooking.behaviour;
-
-import jade.core.behaviours.OneShotBehaviour;
-
-public class MyOneShotBehaviour extends OneShotBehaviour {
-    public void action() {
-        // perform operation X
-    }
-}
